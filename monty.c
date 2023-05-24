b8cfe24e6210655e2ee8fd7ca0813183f4a711c7
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-24 04:48:22 -0700

    New files

---
+++ monty.c
@@ -0,0 +1,152 @@
+#include "monty.h"
+
+/**
+ * main - This is the entry point
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ * Return: This returns 0 on success and exits with EXIT_SUCCESS on failure
+ */
+int main(int argc, char **argv)
+{
+	char buff[1024];
+	FILE *ptr;
+	unsigned int line_number = 0;
+	char *opcode;
+	char *op_val;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	ptr = fopen(argv[1], "r");
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(buff, sizeof(buff), ptr) != NULL)
+	{
+		line_number++;
+		opcode = strtok(buff, " \t\n");
+		op_val = strtok(NULL, " \t\n");
+
+		fnd_func(opcode, line_number)(&stack, line_number, op_val);
+	}
+	fclose(ptr);
+	free_stack(stack);
+	return (0);
+}
+
+/**
+ * *fnd_func - This function finds the corresponding
+ * function based on the opcode given
+ *
+ * @s: This contains the opcode gotten from a .m file
+ * @line: This is the current line of the file
+ * Return: This returns a pointer to the function the corresponds
+ * with the opcode
+ */
+void (*fnd_func(char *s, unsigned int line))(stack_t **, unsigned int, char *)
+{
+	int i;
+
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+
+	for (i = 0; instructions[i].opcode != NULL; i++)
+	{
+		if (strcmp(s, instructions[i].opcode) == 0)
+		{
+			return (instructions[i].f);
+		}
+	}
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", line, s);
+	exit(EXIT_FAILURE);
+
+	return (NULL);
+}
+
+/**
+ * push - This function pushes values unto the stack
+ * @stack: This is a pointer to the top of the stack
+ * @line_number: This is the current line of the file being read from
+ * @val: This is the value to be pushed unto the stack
+ */
+void push(stack_t **stack, unsigned int line_number, char *val)
+{
+	stack_t *new;
+	int value;
+
+	if (val == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = atoi(val);
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new->n = value;
+	new->prev = NULL;
+	new->next = (*stack);
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new;
+	}
+
+	(*stack) = new;
+}
+
+/**
+ * pall - This function prints all the values of the stack out
+ * @stack: This is a pointer to the top of the stack
+ * @line_number: This is the current line of the file being read from
+ * @val: This is the value to be pushed unto the stack
+ */
+void pall(stack_t **stack, unsigned int line_number, char *val)
+{
+	stack_t *current;
+	(void)val;
+	(void)line_number;
+
+	if (*stack == NULL)
+	{
+		return;
+	}
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+ * free_stack - This function frees all allocated memory of the stack_t
+ * @stack: This is a pointer to the top of the stack
+ */
+void free_stack(stack_t *stack)
+{
+	while (stack != NULL)
+	{
+		free(stack);
+		stack = stack->next;
+	}
+}
